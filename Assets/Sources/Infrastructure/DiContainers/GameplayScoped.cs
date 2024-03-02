798513eec3027a9ed4bb733e18d1e7978c88406b
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-03-02 22:09:19 +0300

    Add plaguin VContainer

---
+++ Assets/Sources/Infrastructure/DiContainers/GameplayScoped.cs
@@ -0,0 +1,12 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Sources.Infrastructure.DiContainers
+{
+    public class GameplayScoped : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
